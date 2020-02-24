156538d9c195d75586c7e8fcd030669de0eb3806
davidvido <61408357+davidvido@users.noreply.github.com>
2020-02-24 16:16:26 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Khong Copy Nhe, Thay Hay Thi Vao Day : <a href='http://goldensmile.vn'>http://goldensmile.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>KHONG COPY NHU VAY NHE : <a href='http://goldensmile.vn'>http://goldensmile.vn"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
